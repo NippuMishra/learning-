2d032a9aa53fde4ad4388003afd73b70dba9f166
nippu <nippu3670@gmail.com>
2026-05-27 22:15:52 +0530

    code of reverse a number

---
+++ reverse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int reverse = 0, number, remainder;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    while (number != 0) {
+        remainder = number % 10;
+        reverse = reverse * 10 + remainder;
+        number = number / 10;
+    }
+    printf("Reversed integer: %d\n", reverse);
+    return 0;
+}
